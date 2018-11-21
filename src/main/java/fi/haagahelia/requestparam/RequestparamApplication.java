2eee2d0019ba42df78f2e9e114bb8b86492b7ce7
juhahinkula <hinkula@gmail.com>
2018-11-21 09:19:11 +0200

    initial commit

---
+++ src/main/java/fi/haagahelia/requestparam/RequestparamApplication.java
@@ -0,0 +1,12 @@
+package fi.haagahelia.requestparam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RequestparamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RequestparamApplication.class, args);
+	}
+}
---
+++ src/main/java/fi/haagahelia/requestparam/web/HelloController.java
@@ -0,0 +1,5 @@
+package fi.haagahelia.requestparam.web;
+
+public class HelloController {
+
+}
